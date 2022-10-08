5338bd45d5f6f8337111a07826d58722a5231c2a
Manav Rajani <83126261+Manav2626@users.noreply.github.com>
2022-10-09 01:22:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/test/jumanji/QuizQuestionActivity.kt
@@ -0,0 +1,11 @@
+package com.test.jumanji
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+    }
+}
