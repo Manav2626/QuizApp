54fbd631296b75379226566d01be485b85c18e22
Manav Rajani <83126261+Manav2626@users.noreply.github.com>
2022-10-09 02:33:59 +0530

    Day 1

---
+++ app/src/main/java/com/test/jumanji/Question.kt
@@ -0,0 +1,12 @@
+package com.test.jumanji
+
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAns: Int
+)
